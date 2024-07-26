e5fa06dca223094856da924761b628d94e825adf
Jimmy0Alvarez <jimmy0alvarez@outlook.com>
2024-07-26 07:56:57 -0500

    4

--- aseo-ujier/script.js
+++ aseo-ujier/script.js
@@ -61,15 +61,23 @@ document.getElementById('Enviar').onclick = function() {
 	// Enviar
 	if (aseoMar2) {
 		var siAseoMar2 = '%0A-%20'+aseoMar2;
+	} else {
+		var siAseoMar2 = '';
 	}
 	if (aseoJue2) {
 		var siAseoJue2 = '%0A-%20'+aseoJue2;
+	} else {
+		var siAseoJue2 = '';
 	}
 	if (aseoSab2) {
 		var siAseoSab2 = '%0A-%20'+aseoSab2;
+	} else {
+		var siAseoSab2 = '';
 	}
 	if (aseoDom2) {
 		var siAseoDom2 = '%0A-%20'+aseoDom2;
+	} else {
+		var siAseoDom2 = '';
 	}
 	window.open('https://api.whatsapp.com/send/?text=*ASEO%20Y%20UJIER*%0A%0A`Para%20los%20d%C3%ADas:`%0A-%20Martes%20'+dayMar+'%0A-%20Jueves%20'+dayJue+'%0A-%20S%C3%A1bado%20'+daySab+'%0A-%20Domingo%20'+dayDom+'%0A%0A`Aseo:`%0A-%20*Mar.*%20'+aseoMar+siAseoMar2+'%0A-%20*Jue.*%20'+aseoJue+siAseoJue2+'%0A-%20*S%C3%A1b.*%20'+aseoSab+siAseoSab2+'%0A-%20*Dom.*%20'+aseoDom+siAseoDom2+'%0A%0A`Ujier:`%0A-%20*Mar.*%20'+ujierMar+'%0A-%20*Jue.*%20'+ujierJue+'%0A-%20*S%C3%A1b.*%20'+ujierSab+'%0A-%20*Dom.*%20'+ujierDom);
 }
