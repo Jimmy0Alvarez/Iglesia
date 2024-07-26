06f8e782006dde2010678d8e5f5675357051211f
Jimmy0Alvarez <jimmy0alvarez@outlook.com>
2024-07-26 07:19:58 -0500

    Prueba 2

--- aseo-ujier/script.js
+++ aseo-ujier/script.js
@@ -29,18 +29,29 @@ document.getElementById('Enviar').onclick = function() {
 
 	//Aseo
 	var aseoMar = document.getElementById('aseoMar').value;
+	if (document.getElementById('aseoMar2').hasAttribute('disabled')) {
+		document.getElementById('aseoMar2').removeAttribute('name');
+	} else {
+		var aseoMar2 = document.getElementById('aseoMar2').value;
+	}
 	var aseoJue = document.getElementById('aseoJue').value;
+	if (document.getElementById('aseoJue2').hasAttribute('disabled')) {
+		document.getElementById('aseoJeu2').removeAttribute('name');
+	} else {
+		var aseoJue2 = document.getElementById('aseoJue2').value;
+	}
 	var aseoSab = document.getElementById('aseoSab').value;
+	if (document.getElementById('aseoSab2').hasAttribute('disabled')) {
+		document.getElementById('aseoSab2').removeAttribute('name');
+	} else {
+		var aseoSab2 = document.getElementById('aseoSab2').value;
+	}
 	var aseoDom = document.getElementById('aseoDom').value;
-	var Mar2 = document.getElementById('aseoMar2').value;
-	var aseoMar2 = '%0A'+Mar2+'';
-	var Jue2 = document.getElementById('aseoJue2').value;
-	var aseoJue2 = '%0A'+Jue2+'';
-	var Sab2 = document.getElementById('aseoSab2').value;
-	var aseoSab2 = '%0A'+Sab2+'';
-	var Dom2 = document.getElementById('aseoDom2').value;
-	var aseoDom2 = '%0A'+Dom2+'';
-
+	if (document.getElementById('aseoDom2').hasAttribute('disabled')) {
+		document.getElementById('aseoDom2').removeAttribute('name');
+	} else {
+		var aseoDom2 = document.getElementById('aseoDom2').value;
+	}
 	//Ujier
 	var ujierMar = document.getElementById('ujierMar').value;
 	var ujierJue = document.getElementById('ujierJue').value;
@@ -48,5 +59,17 @@ document.getElementById('Enviar').onclick = function() {
 	var ujierDom = document.getElementById('ujierDom').value;
 
 	// Enviar
-	window.open('https://api.whatsapp.com/send/?text=*ASEO%20Y%20UJIER*%0A%0A`Para%20los%20d%C3%ADas:`%0A-%20Martes%20'+dayMar+'%0A-%20Jueves%20'+dayJue+'%0A-%20S%C3%A1bado%20'+daySab+'%0A-%20Domingo%20'+dayDom+'%0A%0A`Aseo:`%0A-%20*Mar.*%20'+aseoMar+aseoMar2+'%0A-%20*Jue.*%20'+aseoJue+'%0A-%20*S%C3%A1b.*%20'+aseoSab+'%0A-%20*Dom.*%20'+aseoDom+'%0A%0A`Ujier:`%0A-%20*Mar.*%20'+ujierMar+'%0A-%20*Jue.*%20'+ujierJue+'%0A-%20*S%C3%A1b.*%20'+ujierSab+'%0A-%20*Dom.*%20'+ujierDom+'');
+	if (aseoMar2) {
+		var siAseoMar2 = '%0A-%20'+aseoMar2;
+	}
+	if (aseoJue2) {
+		var siAseoJue2 = '%0A-%20'+aseoJue2;
+	}
+	if (aseoSab2) {
+		var siAseoSab2 = '%0A-%20'+aseoSab2;
+	}
+	if (aseoDom2) {
+		var siAseoDom2 = '%0A-%20'+aseoDom2;
+	}
+	window.open('https://api.whatsapp.com/send/?text=*ASEO%20Y%20UJIER*%0A%0A`Para%20los%20d%C3%ADas:`%0A-%20Martes%20'+dayMar+'%0A-%20Jueves%20'+dayJue+'%0A-%20S%C3%A1bado%20'+daySab+'%0A-%20Domingo%20'+dayDom+'%0A%0A`Aseo:`%0A-%20*Mar.*%20'+aseoMar+aseoMar2+'%0A-%20*Jue.*%20'+aseoJue+aseoJue2+'%0A-%20*S%C3%A1b.*%20'+aseoSab+aseoSab2+'%0A-%20*Dom.*%20'+aseoDom+aseoDom2+'%0A%0A`Ujier:`%0A-%20*Mar.*%20'+ujierMar+'%0A-%20*Jue.*%20'+ujierJue+'%0A-%20*S%C3%A1b.*%20'+ujierSab+'%0A-%20*Dom.*%20'+ujierDom);
 }
