4cba33bb989bb84d1d2e79ad8124a7087145e8d0
Jimmy0Alvarez <jimmy0alvarez@outlook.com>
2024-07-31 17:31:31 -0500

    41

--- aseo-ujier/script.js
+++ aseo-ujier/script.js
@@ -1,174 +1,134 @@
 // Botones de Aseo
 document.getElementById('showAseoMar2').onclick = function() {
-	document.getElementById('content-aseoMar2').style.display = 'inline-block'
-	document.getElementById('aseoMar2').required = true
-	document.getElementById('showAseoMar2').style.display = "none";
+	document.getElementById('content-aseoMar2').style.display = 'inline-block';
+	document.getElementById('aseoMar2').required = true;
+	document.getElementById('showAseoMar2').style.display = 'none';
 	document.getElementById('hiddenAseoMar2').onclick = function () {
-		document.getElementById('content-aseoMar2').style.display = "none"
-		document.getElementById('aseoMar2').required = false
-		document.getElementById('showAseoMar2').style.display = "inline-block"
+		document.getElementById('content-aseoMar2').style.display = 'none';
+		document.getElementById('aseoMar2').required = false;
+		document.getElementById('showAseoMar2').style.display = 'inline-block';
 	}
 }
 document.getElementById('showAseoJue2').onclick = function() {
-	document.getElementById('content-aseoJue2').style.display = 'inline-block'
-	document.getElementById('aseoJue2').required = true
-	document.getElementById('showAseoJue2').style.display = "none"
+	document.getElementById('content-aseoJue2').style.display = 'inline-block';
+	document.getElementById('aseoJue2').required = true;
+	document.getElementById('showAseoJue2').style.display = 'none';
 	document.getElementById('hiddenAseoJue2').onclick = function () {
-		document.getElementById('content-aseoJue2').style.display = "none";
-		document.getElementById('aseoJue2').required = false
-		document.getElementById('showAseoJue2').style.display = "inline-block"
+		document.getElementById('content-aseoJue2').style.display = 'none';
+		document.getElementById('aseoJue2').required = false;
+		document.getElementById('showAseoJue2').style.display = 'inline-block';
 	}
 }
 document.getElementById('showAseoSab2').onclick = function() {
-	document.getElementById('content-aseoSab2').style.display = 'inline-block'
-	document.getElementById('aseoSab2').required = true
-	document.getElementById('showAseoSab2').style.display = "none"
+	document.getElementById('content-aseoSab2').style.display = 'inline-block';
+	document.getElementById('aseoSab2').required = true;
+	document.getElementById('showAseoSab2').style.display = 'none';
 	document.getElementById('hiddenAseoSab2').onclick = function () {
-		document.getElementById('content-aseoSab2').style.display = "none"
-		document.getElementById('aseoSab2').required = false
-		document.getElementById('showAseoSab2').style.display = "inline-block"
+		document.getElementById('content-aseoSab2').style.display = 'none';
+		document.getElementById('aseoSab2').required = false;
+		document.getElementById('showAseoSab2').style.display = 'inline-block';
 	}
 }
 document.getElementById('showAseoDom2').onclick = function() {
-	document.getElementById('content-aseoDom2').style.display = 'inline-block'
-	document.getElementById('aseoDom2').required = true
-	document.getElementById('showAseoDom2').style.display = "none"
+	document.getElementById('content-aseoDom2').style.display = 'inline-block';
+	document.getElementById('aseoDom2').required = true;
+	document.getElementById('showAseoDom2').style.display = 'none';
 	document.getElementById('hiddenAseoDom2').onclick = function () {
-		document.getElementById('content-aseoDom2').style.display = "none"
-		document.getElementById('aseoDom2').required = false
-		document.getElementById('showAseoDom2').style.display = "inline-block"
+		document.getElementById('content-aseoDom2').style.display = 'none';
+		document.getElementById('aseoDom2').required = false;
+		document.getElementById('showAseoDom2').style.display = 'inline-block';
 	}
 }
 // Botones de Ujier
 document.getElementById('showUjierMar2').onclick = function() {
-	document.getElementById('content-ujierMar2').style.display = 'inline-block'
-	document.getElementById('ujierMar2').required = true
-	document.getElementById('showUjierMar2').style.display = "none";
+	document.getElementById('content-ujierMar2').style.display = 'inline-block';
+	document.getElementById('ujierMar2').required = true;
+	document.getElementById('showUjierMar2').style.display = 'none';
 	document.getElementById('hiddenUjierMar2').onclick = function () {
-		document.getElementById('content-ujierMar2').style.display = "none"
-		document.getElementById('ujierMar2').required = false
-		document.getElementById('showUjierMar2').style.display = "inline-block"
+		document.getElementById('content-ujierMar2').style.display = 'none';
+		document.getElementById('ujierMar2').required = false;
+		document.getElementById('showUjierMar2').style.display = 'inline-block';
 	}
 }
 document.getElementById('showUjierJue2').onclick = function() {
-	document.getElementById('content-ujierJue2').style.display = 'inline-block'
-	document.getElementById('ujierJue2').required = true
-	document.getElementById('showUjierJue2').style.display = "none"
+	document.getElementById('content-ujierJue2').style.display = 'inline-block';
+	document.getElementById('ujierJue2').required = true;
+	document.getElementById('showUjierJue2').style.display = 'none';
 	document.getElementById('hiddenUjierJue2').onclick = function () {
-		document.getElementById('content-ujierJue2').style.display = "none";
-		document.getElementById('ujierJue2').required = false
-		document.getElementById('showUjierJue2').style.display = "inline-block"
+		document.getElementById('content-ujierJue2').style.display = 'none';
+		document.getElementById('ujierJue2').required = false;
+		document.getElementById('showUjierJue2').style.display = 'inline-block';
 	}
 }
 document.getElementById('showUjierSab2').onclick = function() {
-	document.getElementById('content-ujierSab2').style.display = 'inline-block'
-	document.getElementById('ujierSab2').required = true
-	document.getElementById('showUjierSab2').style.display = "none"
+	document.getElementById('content-ujierSab2').style.display = 'inline-block';
+	document.getElementById('ujierSab2').required = true;
+	document.getElementById('showUjierSab2').style.display = 'none';
 	document.getElementById('hiddenUjierSab2').onclick = function () {
-		document.getElementById('content-ujierSab2').style.display = "none"
-		document.getElementById('ujierSab2').required = false
-		document.getElementById('showUjierSab2').style.display = "inline-block"
+		document.getElementById('content-ujierSab2').style.display = 'none';
+		document.getElementById('ujierSab2').required = false;
+		document.getElementById('showUjierSab2').style.display = 'inline-block';
 	}
 }
 document.getElementById('showUjierDom2').onclick = function() {
-	document.getElementById('content-ujierDom2').style.display = 'inline-block'
-	document.getElementById('ujierDom2').required = true
-	document.getElementById('showUjierDom2').style.display = "none"
+	document.getElementById('content-ujierDom2').style.display = 'inline-block';
+	document.getElementById('ujierDom2').required = true;
+	document.getElementById('showUjierDom2').style.display = 'none';
 	document.getElementById('hiddenUjierDom2').onclick = function () {
-		document.getElementById('content-ujierDom2').style.display = "none"
-		document.getElementById('ujierDom2').required = false
-		document.getElementById('showUjierDom2').style.display = "inline-block"
+		document.getElementById('content-ujierDom2').style.display = 'none';
+		document.getElementById('ujierDom2').required = false;
+		document.getElementById('showUjierDom2').style.display = 'inline-block';
 	}
 }
 
 // Función Enviar
-document.getElementById('enviar').addEventListener('onclick', function() {
-	window.open('https://api.whatsapp.com/send/?text=*ASEO%20Y%20UJIER*%0A%0A`Para%20los%20d%C3%ADas:`%0A-%20Martes%20'+dayMar+'%0A-%20Jueves%20'+dayJue+'%0A-%20S%C3%A1bado%20'+daySab+'%0A-%20Domingo%20'+dayDom+'%0A%0A`Aseo:`%0A-%20*Mar.*%20'+aseoMar+siAseoMar2+'%0A-%20*Jue.*%20'+aseoJue+siAseoJue2+'%0A-%20*S%C3%A1b.*%20'+aseoSab+siAseoSab2+'%0A-%20*Dom.*%20'+aseoDom+siAseoDom2+'%0A%0A`Ujier:`%0A-%20*Mar.*%20'+ujierMar+siUjierMar2+'%0A-%20*Jue.*%20'+ujierJue+siUjierJue2+'%0A-%20*S%C3%A1b.*%20'+ujierSab+siUjierSab2+'%0A-%20*Dom.*%20'+ujierDom+siUjierDom2)
+document.getElementById('enviar').addEventListener('click', function() {
 	//Fecha del Martes, Jueves, Sabado y domingo
-	var dateMartes = document.getElementById('dateMar').value
-	var dateMart = new Date(dateMartes)
-	var dayMar = dateMart.getDate() + 1
-	var dateJueves = document.getElementById('dateJue').value
-	var dateJue = new Date(dateJueves)
-	var dayJue = dateJue.getDate() + 1
-	var dateSabado = document.getElementById('dateSab').value
-	var dateSab = new Date(dateSabado)
-	var daySab = dateSab.getDate() + 1
-	var dateDomingo = document.getElementById('dateDom').value
-	var dateDom = new Date(dateDomingo)
-	var dayDom = dateDom.getDate() + 1
+	var dateMartes = document.getElementById('dateMar').value;
+	var dateMart = new Date(dateMartes);
+	var dayMar = dateMart.getDate() + 1;
+	var dateJueves = document.getElementById('dateJue').value;
+	var dateJue = new Date(dateJueves);
+	var dayJue = dateJue.getDate() + 1;
+	var dateSabado = document.getElementById('dateSab').value;
+	var dateSab = new Date(dateSabado);
+	var daySab = dateSab.getDate() + 1;
+	var dateDomingo = document.getElementById('dateDom').value;
+	var dateDom = new Date(dateDomingo);
+	var dayDom = dateDom.getDate() + 1;
 
 	// Aseo Campus 1
-	var aseoMar = document.getElementById('aseoMar').value
-	var aseoJue = document.getElementById('aseoJue').value
-	var aseoSab = document.getElementById('aseoSab').value
-	var aseoDom = document.getElementById('aseoDom').value
+	var aseoMar = document.getElementById('aseoMar').value;
+	var aseoJue = document.getElementById('aseoJue').value;
+	var aseoSab = document.getElementById('aseoSab').value;
+	var aseoDom = document.getElementById('aseoDom').value;
 
 	// Aseo Campus 2
-	if (document.getElementById('aseoMar2').value !== '' && document.getElementById('content-aseoMar2').style.display == 'inline-block'){
-		var aseoMar2 = document.getElementById('aseoMar2').value
-		var siAseoMar2 = '%0A-%20'+aseoMar2
-	} else {
-		document.getElementById('aseoMar2').removeAttribute('name')
-		var siAseoMar2 = ''
-	}
-	if (document.getElementById('aseoJue2').value !== '') {
-		var aseoJue2 = document.getElementById('aseoJue2').value
-		var siAseoJue2 = '%0A-%20'+aseoJue2
-	} else {
-		document.getElementById('aseoJue2').removeAttribute('name')
-		var siAseoJue2 = ''
-	}
-	if (document.getElementById('aseoSab2').value !== '') {
-		var aseoSab2 = document.getElementById('aseoSab2').value
-		var siAseoSab2 = '%0A-%20'+aseoSab2
-	} else {
-		document.getElementById('aseoSab2').removeAttribute('name')
-		var siAseoSab2 = ''
-	}
-	if (document.getElementById('aseoDom2').value !== '') {
-		var aseoDom2 = document.getElementById('aseoDom2').value
-		var siAseoDom2 = '%0A-%20'+aseoDom2
-	} else {
-		document.getElementById('aseoDom2').removeAttribute('name')
-		var siAseoDom2 = ''
-	}
+	var aseoMar2 = document.getElementById('aseoMar2').value !== '' && document.getElementById('content-aseoMar2').style.display === 'inline-block' ? '%0A-%20'+document.getElementById('aseoMar2').value : '';
+	if (aseoMar2 === ''){document.getElementById('aseoMar2').removeAttribute('name')};
+	var aseoJue2 = document.getElementById('aseoJue2').value !== '' && document.getElementById('content-aseoJue2').style.display === 'inline-block' ? '%0A-%20'+document.getElementById('aseoJue2').value : '';
+	if (aseoJue2 === ''){document.getElementById('aseoJue2').removeAttribute('name')};
+	var aseoSab2 = document.getElementById('aseoSab2').value !== '' && document.getElementById('content-aseoSab2').style.display === 'inline-block' ? '%0A-%20'+document.getElementById('aseoSab2').value : '';
+	if (aseoSab2 === ''){document.getElementById('aseoSab2').removeAttribute('name')};
+	var aseoDom2 = document.getElementById('aseoDom2').value !== '' && document.getElementById('content-aseoDom2').style.display === 'inline-block' ? '%0A-%20'+document.getElementById('aseoDom2').value : '';
+	if (aseoDom2 === ''){document.getElementById('aseoDom2').removeAttribute('name')};
 	
 	//Ujier Campus 1
-	var ujierMar = document.getElementById('ujierMar').value
-	var ujierJue = document.getElementById('ujierJue').value
-	var ujierSab = document.getElementById('ujierSab').value
-	var ujierDom = document.getElementById('ujierDom').value
+	var ujierMar = document.getElementById('ujierMar').value;
+	var ujierJue = document.getElementById('ujierJue').value;
+	var ujierSab = document.getElementById('ujierSab').value;
+	var ujierDom = document.getElementById('ujierDom').value;
 
 	// Ujier Campus 2
-	if (document.getElementById('ujierMar2').value !== '') {
-		var ujierMar2 = document.getElementById('ujierMar2').value
-		var siUjierMar2 = '%0A-%20'+ujierMar2
-	} else {
-		document.getElementById('ujierMar2').removeAttribute('name')
-		var siUjierMar2 = ''
-	}
-	if (document.getElementById('ujierJue2').value !== '') {
-		var ujierJue2 = document.getElementById('aujierue2').value
-		var siUjierJue2 = '%0A-%20'+ujierJue2
-	} else {
-		document.getElementById('ujierJue2').removeAttribute('name')
-		var siUjierJue2 = ''
-	}
-	if (document.getElementById('ujierSab2').value !== '') {
-		var ujierSab2 = document.getElementById('ujierSab2').value
-		var siUjierSab2 = '%0A-%20'+ujierSab2
-	} else {
-		document.getElementById('ujierSab2').removeAttribute('name')
-		var siUjierSab2 = ''
-	}
-	if (document.getElementById('ujierDom2').value !== '') {
-		var ujierDom2 = document.getElementById('ujierDom2').value
-		var siUjierDom2 = '%0A-%20'+ujierDom2
-	} else {
-		document.getElementById('ujierDom2').removeAttribute('name')
-		var siUjierDom2 = ''
-	}
+	var ujierMar2 = document.getElementById('ujierMar2').value !== '' && document.getElementById('content-ujierMar2').style.display === 'inline-block' ? '%0A-%20'+document.getElementById('ujierMar2').value : '';
+	if (ujierMar2 === ''){document.getElementById('ujierMar2').removeAttribute('name')};
+	var ujierJue2 = document.getElementById('ujierJue2').value !== '' && document.getElementById('content-ujierJue2').style.display === 'inline-block' ? '%0A-%20'+document.getElementById('ujierJue2').value : '';
+	if (ujierJue2 === ''){document.getElementById('ujierJue2').removeAttribute('name')};
+	var ujierSab2 = document.getElementById('ujierSab2').value !== '' && document.getElementById('content-ujierSab2').style.display === 'inline-block' ? '%0A-%20'+document.getElementById('ujierSab2').value : '';
+	if (ujierSab2 === ''){document.getElementById('ujierSab2').removeAttribute('name')};
+	var ujierDom2 = document.getElementById('ujierDom2').value !== '' && document.getElementById('content-ujierDom2').style.display === 'inline-block' ? '%0A-%20'+document.getElementById('ujierDom2').value : '';
+	if (ujierDom2 === ''){document.getElementById('ujierDom2').removeAttribute('name')};
 	
 	// Enviar
+	window.open('https://api.whatsapp.com/send/?text=*ASEO%20Y%20UJIER*%0A%0A`Para%20los%20d%C3%ADas:`%0A-%20Martes%20'+dayMar+'%0A-%20Jueves%20'+dayJue+'%0A-%20S%C3%A1bado%20'+daySab+'%0A-%20Domingo%20'+dayDom+'%0A%0A`Aseo:`%0A-%20*Mar.*%20'+aseoMar+aseoMar2+'%0A-%20*Jue.*%20'+aseoJue+aseoJue2+'%0A-%20*S%C3%A1b.*%20'+aseoSab+aseoSab2+'%0A-%20*Dom.*%20'+aseoDom+aseoDom2+'%0A%0A`Ujier:`%0A-%20*Mar.*%20'+ujierMar+ujierMar2+'%0A-%20*Jue.*%20'+ujierJue+ujierJue2+'%0A-%20*S%C3%A1b.*%20'+ujierSab+ujierSab2+'%0A-%20*Dom.*%20'+ujierDom+ujierDom2)
 })
